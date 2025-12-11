2d2ccfecd1bcafc1c7249252b04dd01e195dbc9f
Michael Santelia <ecumike@gmail.com>
2025-12-10 21:46:20 -0500

    Fix for local vs gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/connections/',
+  base: mode === "development" ? "/" : "/connections/",
 }));
